fb9adb46f09a5d0168414a955019d9a50b860a0f
Dmitry Vershinin <dima-vers0@rambler.ru>
2019-09-27 12:57:34 +0700

    json

--- src/main/java/com/example/heroku/controller/OldTypeController.java
+++ src/main/java/com/example/heroku/controller/OldTypeController.java
@@ -4,10 +4,13 @@ import com.example.heroku.model.Message;
 import com.example.heroku.repo.MessageRepo;
 import com.example.heroku.utils.MessageUtil;
 import com.example.heroku.utils.UtilsForWeb;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.Map;
@@ -29,12 +32,24 @@ public class OldTypeController {
         return "index";
     }
 
+    @RequestMapping("/json")
+    @ResponseBody
+    String json(){
+        ObjectMapper obj = new ObjectMapper();
+        try {
+            return obj.writeValueAsString(messageRepo.findAll());
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return "null";
+    }
+
     @RequestMapping("/db")
     String db(ModelMap modelMap) {
         modelMap.addAttribute("utils", new UtilsForWeb());
-        modelMap.addAttribute("message", new MessageUtil("danger", "Something wrong..."));
+        modelMap.addAttribute("message", new MessageUtil("success", "Something text"));
         modelMap.addAttribute("records", messageRepo.findAll());
-        messageRepo.saveAndFlush(new Message("MESSAGE " + messageRepo.findAll().size()));
+//        messageRepo.saveAndFlush(new Message("MESSAGE " + messageRepo.findAll().size()));
         return "db";
     }
 
