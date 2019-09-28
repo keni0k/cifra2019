14d40b3b8645bfa17b8adf8791c870742f2cf26b
Dmitry Vershinin <dima-vers0@rambler.ru>
2019-09-29 05:21:38 +0700

    saveAndFlash

--- src/main/java/com/example/heroku/controller/TubeController.java
+++ src/main/java/com/example/heroku/controller/TubeController.java
@@ -88,10 +88,10 @@ public class TubeController {
                    String gost) {
         Point p1 = new Point(desc1, p1_lat, p1_lon, size1);
         Point p2 = new Point(desc2, p2_lat, p2_lon, size2);
-        pointRepo.save(p1);
-        pointRepo.save(p2);
+        pointRepo.saveAndFlush(p1);
+        pointRepo.saveAndFlush(p2);
         Tube tube = new Tube(p1.getId(), p2.getId(), z_coord, id_owners, gost);
-        tubeRepo.save(tube);
+        tubeRepo.saveAndFlush(tube);
         return "OK";
     }
 
