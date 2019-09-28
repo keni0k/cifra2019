7acf585f0551a5b1eaddc681f5d731171bf0ac68
Dmitry Vershinin <dima-vers0@rambler.ru>
2019-09-28 15:38:17 +0700

    building

--- src/main/java/com/example/heroku/repo/BuildingRepo.java
+++ src/main/java/com/example/heroku/repo/BuildingRepo.java
@@ -2,7 +2,9 @@ package com.example.heroku.repo;
 
 import com.example.heroku.model.Building;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BuildingRepo extends JpaRepository<Building, Long> {
 
     Building getBuildingById(long id);
